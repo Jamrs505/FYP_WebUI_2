f35b02cce1fc7c6403bf46120650cfaea3e830a8
Jamrs <hudsonjames028@gmail.com>
2026-02-12 00:09:57 +0000

    regex

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', () => {
         const direction = document.getElementById('direction').value;
         const destinationIP = document.getElementById('destinationIP').value || 'any';
         const destinationPort = document.getElementById('destinationPort').value || 'any';
-        
+
         //ruleOptionsGeneral
         const ruleMessage = document.getElementById('msg').value;
 
@@ -38,11 +38,30 @@ document.addEventListener('DOMContentLoaded', () => {
         const distance = document.getElementById('distance').value;
         const within = document.getElementById('within').value;
 
+        const bufferlenOperator = document.getElementById('bufferlenOperator').value;
+        const bufferlen = document.getElementById('bufferlen').value;
+        const bufferlenStart = document.getElementById('bufferlenStart').value;
+        const bufferlenRelative = document.getElementById('bufferlenRelative').checked;
+
+        const isdataatNotOperator = document.getElementById('isdataatNotOperator').checked;
+        const isdataat = document.getElementById('isdataat').value;
+        const isdataatRelative = document.getElementById('isdataatRelative').checked;
+
+        const dsizeOperator = document.getElementById('dsizeOperator').value;
+        const dsize = document.getElementById('dsize').value;
+        const dsizeStart = document.getElementById('dsizeStart').value;
+
+        const pcre = document.getElementById('pcre').value;
+        const pcreFast_pattern = document.getElementById('pcreFast_pattern').checked;
+        const pcreNocase = document.getElementById('pcreNocase').checked;
+
+
+
         let rule = `${action} ${protocol} ${sourceIP} ${sourcePort} ${direction} ${destinationIP} ${destinationPort} (`;
 
         if (ruleMessage) rule += `msg:"${ruleMessage}"; `;
 
-        if (reference && referenceText) rule += `reference:${reference},${referenceText}; `;
+        if ((reference !== "Reference") && referenceText) rule += `reference:${reference},${referenceText}; `;
         if (gid) rule += `gid:${gid}; `;
         if (sid) rule += `sid:${sid}; `;
         if (rev) rule += `rev:${rev}; `;
@@ -52,7 +71,14 @@ document.addEventListener('DOMContentLoaded', () => {
         if (service) rule += `service:${service}; `;
         if (rem) rule += `rem:"${rem}"; `;
 
+        //Content options
         if (content) {
+            if (isdataat) {
+                rule += `isdataat:${isdataat}`;
+                if (isdataatRelative) rule += `,relative `;
+                rule += '; ';
+            }
+
             rule += `content:"${content}"`;
             if (fast_pattern) rule += ', fast_pattern';
             if (nocase) rule += ', nocase';
@@ -63,18 +89,62 @@ document.addEventListener('DOMContentLoaded', () => {
             if (distance) rule += `, distance:${distance}`;
             if (within) rule += `, within:${within}`;
             rule += '; ';
+
+            if (bufferlen) {
+                if (bufferlenOperator === '<>' || bufferlenOperator === '<=>') {
+                    rule += `bufferlen:${bufferlenStart}${bufferlenOperator}${bufferlen}`;
+                }
+                else {
+                    rule += `bufferlen:${bufferlen}`;
+                }
+                if (bufferlenRelative) rule += `,relative`;
+                rule += '; ';
+            }
+
+            if (dsize) {
+                if (dsizeOperator === '<>' || dsizeOperator === '<=>') {
+                    rule += `dsize:${dsizeStart}${dsizeOperator}${dsize}`;
+                }
+                else {
+                    rule += `dsize:${dsize}`;
+                }
+                if (dsizeRelative) rule += `,relative`;
+                rule += '; ';
+            }
+
         }
+        
+        if (pcre) {
+                rule += `pcre:"${pcre}"`;
+                if (pcreFast_pattern) rule += ', fast_pattern';
+                if (pcreNocase) rule += ', nocase';
+                rule += '; ';
+            }
+
         rule += ')';
 
-        console.log(action) //Testing
-        console.log(protocol) //Testing
         if (action === 'Action' || protocol === 'Protocol') {
             outputField.value = ''; // Clear output if essential fields are not selected
         } else {
-        outputField.value = rule; }
+            outputField.value = rule;
+        }
+
+
+        // Hide or show bufferlenStart based on bufferlen operator
+        if (bufferlenOperator == '<>' || bufferlenOperator == '<=>') {
+            document.getElementById('bufferlenStart').hidden = false;
+        } else {
+            document.getElementById('bufferlenStart').hidden = true;
+        }
+
+        // Hide or show dsizeStart based on dsize operator
+        if (dsizeOperator == '<>' || dsizeOperator == '<=>') {
+            document.getElementById('dsizeStart').hidden = false;
+        } else {
+            document.getElementById('dsizeStart').hidden = true;
+        }
+
     }
-
-
 
     inputs.forEach(input => {
         input.addEventListener('input', generateRule);
